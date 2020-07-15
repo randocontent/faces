55b6b31c2a3e06eee7e8226930eec7169462bc40
jspong0 <jspong0@>
2020-07-15 15:27:30 +0300

    pose loaded

--- pose1/sketch.js
+++ pose1/sketch.js
@@ -1,4 +1,7 @@
 let img, capture, startButton, posePreview;
+let poseNet;
+let poses = [];
+
 
 function startWebcam() {
 	capture = createCapture(VIDEO);
@@ -9,7 +12,9 @@ function startWebcam() {
 }
 
 function setup() {
-	var canvas = createCanvas(400,400);
+	
+	let elWidth = select('#sketch-placeholder').width;
+	var canvas = createCanvas(elWidth,elWidth*0.9);
 	canvas.parent('sketch-placeholder');
 	startWebcam();
 
@@ -18,13 +23,59 @@ function setup() {
 
 	startButton = select('#redo')
 	startButton.mousePressed(startWebcam)
+	
+	poseNet = ml5.poseNet(capture, modelReady);
+	poseNet.on('pose', function(results) {
+    poses = results;
+	});
+	
+}
+
+function modelReady() {
+  select('#status').html('Model Loaded');
 }
 
 function draw() {
-	background(255);
+	background(225);
 	if (capture) {
 		// image(capture, 100, 100, 320, 240);
 		
 	}
-	filter(POSTERIZE, 2);
+	// filter(POSTERIZE, 2);
+	drawKeypoints();
+  drawSkeleton();
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints()  {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
+    // For each pose detected, loop through all the keypoints
+    let pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      if (keypoint.score > 0.2) {
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      }
+    }
+  }
+}
+
+// A function to draw the skeletons
+function drawSkeleton() {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i++) {
+    let skeleton = poses[i].skeleton;
+    // For every skeleton, loop through all body connections
+    for (let j = 0; j < skeleton.length; j++) {
+      let partA = skeleton[j][0];
+      let partB = skeleton[j][1];
+      stroke(255, 0, 0);
+      line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+    }
+  }
 }
