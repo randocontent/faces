6c1b8df441cddd7244c4b53904a5c74b97836461
jspong0 <jspong0@>
2020-08-06 03:17:41 +0300

    waveforms 2

--- waveforms2/sketch.js
+++ waveforms2/sketch.js
@@ -1,7 +1,6 @@
 let mic, recorder, soundFile, fft;
 let song;
 let recordingState = 0;
-let micLevel;
 
 let yoff = 0.0;
 
@@ -18,7 +17,6 @@ function setup() {
 
 	fft = new p5.FFT();
 	fft.setInput(mic);
-
 	tape();
 }
 
@@ -27,44 +25,42 @@ function windowResized() {
 }
 
 function draw() {
-	micLevel = mic.getLevel()
 	let spectrum = fft.analyze();
 
 	background(200);
 	noStroke();
 
-	
 	let step = 16;
-	
-	for (let i = 0; i < spectrum.length; i += step) {
+
+	for (let i = 0; i < spectrum.length; i = i + step) {
 		// Map i (which should be 1024) to 360
 		// and use it for the bar's hue
 		colorMode(HSB);
 		let x = map(i, 0, spectrum.length / 1.8, 0, width);
 		let hue = map(i, 0, spectrum.length / 1.8, 0, 340);
 		fill(hue, 200, 80, 255);
-		
+
 		let level = map(spectrum[i], 0, 255, height, 0);
 		let barWidth = width / step / 5;
 		rect(x, level, barWidth, height);
 	}
-	
+
 	noFill();
 	beginShape();
-	
+
 	let xoff = 0; // Option #1: 2D Noise
 	// let xoff = yoff; // Option #2: 1D Noise
-	
+
 	// Iterate over horizontal pixels
 	for (let x = 0; x <= width; x += 10) {
 		// Calculate a y value according to noise, map to
-		
+
 		// Option #1: 2D Noise
 		let y = map(noise(xoff, yoff), 0, 1, 200, 300);
-		
+
 		// Option #2: 1D Noise
 		// let y = map(noise(xoff), 0, 1, 200,300);
-		
+
 		// Set the vertex
 		vertex(x, y);
 		// Increment x dimension for noise
@@ -75,12 +71,6 @@ function draw() {
 	vertex(width, height);
 	vertex(0, height);
 	endShape(CLOSE);
- 
-	push()
-	fill(50)
-	ellipse(width/2,height/2,.5/micLevel)
-
-	pop()
 }
 
 function tape() {
